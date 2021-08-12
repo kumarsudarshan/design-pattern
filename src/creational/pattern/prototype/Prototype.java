d22297a41bb3f6c9e4ffa58e2e84e0c1118c8629
kumarsudarshan <ksudarsh@in.ibm.com>
2021-08-12 11:25:56 +0530

    Prototype Pattern

---
+++ src/creational/pattern/prototype/Prototype.java
@@ -0,0 +1,5 @@
+package creational.pattern.prototype;
+
+public interface Prototype {
+    public Prototype getClone();
+}
---
+++ src/creational/pattern/prototype/PrototypeDemo.java
@@ -0,0 +1,20 @@
+package creational.pattern.prototype;
+
+
+class PrototypeDemo {
+    public static void main(String[] args) {
+
+        int eid = 111;
+        String ename = "Kumar";
+        String edesignation = "Developer";
+        String eaddress = "Bangalore";
+        double esalary = 2600;
+
+        EmployeeRecord e1 = new EmployeeRecord(eid, ename, edesignation, esalary, eaddress);
+
+        e1.showRecord();
+        System.out.println("\n");
+        EmployeeRecord e2 = (EmployeeRecord) e1.getClone();
+        e2.showRecord();
+    }
+}
