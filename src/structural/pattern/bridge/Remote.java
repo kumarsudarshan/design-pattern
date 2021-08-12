4cb8e9e1af40d6dbb1f93fb0518ae58dd0a162af
kumarsudarshan <ksudarsh@in.ibm.com>
2021-08-12 21:36:06 +0530

    Bridge Pattern

---
+++ src/structural/pattern/bridge/Remote.java
@@ -0,0 +1,13 @@
+package structural.pattern.bridge;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
